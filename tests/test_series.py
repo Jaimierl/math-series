e45fbe08402a2b8ded423320b80c6c5e3b488a58
Jae Loney <jaimieloney@gmail.com>
2022-05-19 00:37:37 -0400

    tests

--- tests/test_series.py
+++ tests/test_series.py
@@ -4,55 +4,55 @@ from series.series import sum_series
 
 # To make testing run remember you need to start up your virtual environment and pip install pytest into your virtual environment.
 
-# def test_fibonacci_zero():
-#     actual = fibonacci(0)
-#     expected = 0
-#     assert actual == expected
+def test_fibonacci_zero():
+    actual = fibonacci(0)
+    expected = 0
+    assert actual == expected
 
-# def test_fibonacci_one():
-#     actual = fibonacci(1)
-#     expected = 1
-#     assert actual == expected
+def test_fibonacci_one():
+    actual = fibonacci(1)
+    expected = 1
+    assert actual == expected
 
-# def test_fibonacci_two():
-#     actual = fibonacci(2)
-#     expected = 1
-#     assert actual == expected
+def test_fibonacci_two():
+    actual = fibonacci(2)
+    expected = 1
+    assert actual == expected
 
-# def test_fibonacci_five():
-#     actual = fibonacci(5)
-#     expected = 5
-#     assert actual == expected
+def test_fibonacci_five():
+    actual = fibonacci(5)
+    expected = 5
+    assert actual == expected
 
-# def test_fibonacci_negative():
-#     actual = fibonacci(-27)
-#     expected = ("Negative numbers are not supported")
-#     assert actual == expected
+def test_fibonacci_negative():
+    actual = fibonacci(-27)
+    expected = ("Negative numbers are not supported")
+    assert actual == expected
 
-# def test_lucas_zero():
-#     actual = lucas(0)
-#     expected = 2
-#     assert actual == expected
+def test_lucas_zero():
+    actual = lucas(0)
+    expected = 2
+    assert actual == expected
 
-# def test_lucas_one():
-#     actual = lucas(1)
-#     expected = 1
-#     assert actual == expected
+def test_lucas_one():
+    actual = lucas(1)
+    expected = 1
+    assert actual == expected
 
-# def test_lucas_two():
-#     actual = lucas(2)
-#     expected = 3
-#     assert actual == expected
+def test_lucas_two():
+    actual = lucas(2)
+    expected = 3
+    assert actual == expected
 
-# def test_lucas_five():
-#     actual = lucas(5)
-#     expected = 11
-#     assert actual == expected
+def test_lucas_five():
+    actual = lucas(5)
+    expected = 11
+    assert actual == expected
 
-# def test_lucas_negative():
-#     actual = lucas(-3)
-#     expected = ("Negative numbers are not supported")
-#     assert  actual == expected
+def test_lucas_negative():
+    actual = lucas(-3)
+    expected = ("Negative numbers are not supported")
+    assert  actual == expected
 
 def test_sum_series_fibonacci():
     actual = sum_series(5)
