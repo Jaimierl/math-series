57a997aff435734750ac4c1b83f2cfb605539fcc
Jae Loney <jaimieloney@gmail.com>
2022-05-18 22:23:57 -0400

    tests

---
+++ series.py
@@ -0,0 +1,21 @@
+def fibonacci(n):
+    """Fibonacci Function
+
+    Args:
+        n 
+    
+    This function adds the previous numbers from the base numbers of 0,1 adding the numbers before it together until the nth number in the series.
+    """
+    if n<=0:
+        return n
+    else:
+        return ("Great")
+
+def lucas(n):
+    """Lucas Function
+
+    Args:
+        n 
+    
+    This function adds the previous numbers from the base numbers of 2,1 adding the numbers before it together until the nth number in the series.
+    """
